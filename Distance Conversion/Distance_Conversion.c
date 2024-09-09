46c36eb398f3c1434eb0045311ff556167d88199
Nizar Beriane <nizarberiane43@gmail.com>
2024-09-09 17:04:18 +0100

    Distance Conversion

---
+++ Distance Conversion/Distance_Conversion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+float km;
+float yard;
+
+int main() {
+
+    printf("enter the distance in kilometers (km): ");
+    scanf("%f", &km);
+
+    yard = (km * 1093.61);
+    printf("the distance in yard is : %.2f", yard);
+    
+    return 0;
+}
