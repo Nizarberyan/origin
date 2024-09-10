1730fc9a5b9dd701a26926b0ff3e85331d681b50
Nizar Beriane <nizarberiane43@gmail.com>
2024-09-10 09:30:26 +0100

    Correction

--- Temperature Conversion/Temperature_Conversion.c
+++ Temperature Conversion/Temperature_Conversion.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int C, K;
+int C;
+float K;
 
 int main()
 {
-    printf("enter the temperature")
+    printf("enter the temperature: ");
+    scanf("%d", &C);
+    K = (C + 273.15);
+
+    printf("the temperature in Kelvin is: %.2f", K);
+    return 0;
 }
