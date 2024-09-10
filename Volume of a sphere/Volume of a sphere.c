073155016d14c594165705a2ccc3d43ba0438137
Nizar Beriane <nizarberiane43@gmail.com>
2024-09-10 10:38:27 +0100

    Volume of a sphere

---
+++ Volume of a sphere/Volume of a sphere.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+
+float r,V;
+const float Pi=3.14159;
+
+printf("enter the radius of the sphere: ");
+scanf("%f",&r);
+
+V = (4.0/3.0) * Pi * pow(r,3);
+
+printf("The volume of the sphere is: %.2f cubic units\n",V);
+
+return 0;
+}
