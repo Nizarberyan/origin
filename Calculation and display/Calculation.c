7362e8bdc5c4044a5c4a26c592f670792c23c815
Nizar Beriane <nizarberiane43@gmail.com>
2024-09-10 02:11:07 +0100

    Calculation

---
+++ Calculation and display/Calculation.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+
+    printf("Enter the first number (a): ");
+    scanf("%f", &a);
+    printf("Enter the second number (b): ");
+    scanf("%f", &b);
+
+
+    printf("a + b = %.2f\n", a + b);
+    printf("a - b = %.2f\n", a - b);
+    printf("a * b = %.2f\n", a * b);
+
+
+    if (b != 0.0) {
+        printf("a / b = %.2f\n", a / b);
+    } else {
+        printf("Error: Division by zero\n");
+    }
+
+    return 0;
+}
